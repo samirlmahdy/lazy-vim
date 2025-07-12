bf384e7b95e1691f7e3c60bf665e09e77ca906c7
Samir Elmahdy <126981505+samirlmahdy@users.noreply.github.com>
2025-07-12 14:56:43 +0300

    changes

---
+++ lua/plugins/leetcode.lua
@@ -0,0 +1,12 @@
+return {
+    "kawre/leetcode.nvim",
+    dependencies = {
+        -- include a picker of your choice, see picker section for more details
+        "nvim-lua/plenary.nvim",
+        "MunifTanjim/nui.nvim",
+    },
+    opts = {
+        -- configuration goes here
+        lang = "typescript",
+    },
+}
